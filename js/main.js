b472956263bc8f82c934a9e3898748aa7c92fad0
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-10 20:10:47 +0100

    script(): reset

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@
 // let price;
 
 // Output
-const send = document.getElementById(`send`);
-const discount = document.getElementById(`discount`);
-const carriage = document.getElementById(`carriage`);
-const code = document.getElementById(`code`);
-const price = document.getElementById(`price`);
-
+// const send = document.getElementById(`send`);
+// const discount = document.getElementById(`discount`);
+// const carriage = document.getElementById(`carriage`);
+// const code = document.getElementById(`code`);
+// const price = document.getElementById(`price`);
+let offerta;
 
 send.addEventListener(`click`, function(){
 
-    const name = document.getElementById(`name`);
+    const name = document.getElementById(`name`).value;
     
     const km = parseInt(document.getElementById(`km`).value);
     console.log(km);
@@ -30,8 +30,38 @@ send.addEventListener(`click`, function(){
     let partialprice = km * 0.21;
     console.log(partialprice);
 
-    if(age == minorenne){
-        
+    if(age == `minorenne`){
+        finalprice = partialprice * 0.2;
+        offerta = `20% di sconto`;
+        document.getElementById(`nome`).innerText = name;
+        document.getElementById(`discount`).innerText = offerta;
+        document.getElementById(`price`).innerText = finalprice.toFixed(2) + ` €`;
+        document.getElementById(`carriage`).innerText = Math.floor(Math.random()* 20 + 1);
+        document.getElementById(`code`).innerText = Math.floor(Math.random()* 99999 + 1);
     }
+    else if(age == `senior`){
+        finalprice = partialprice * 0.4;
+        offerta = `40% di sconto`;
+        document.getElementById(`nome`).innerText = name;
+        document.getElementById(`discount`).innerText = offerta;
+        document.getElementById(`price`).innerText = finalprice.toFixed(2) + ` €`;
+        document.getElementById(`carriage`).innerText = Math.floor(Math.random()* 20 + 1);
+        document.getElementById(`code`).innerText = Math.floor(Math.random()* 99999 + 1);
+    }
+    else if (age == `maggiorenne`){
+        finalprice = partialprice;
+        offerta = `Tariffa base`;
+        document.getElementById(`nome`).innerText = name;
+        document.getElementById(`discount`).innerText = offerta;
+        document.getElementById(`price`).innerText = finalprice.toFixed(2) + ` €`;
+        document.getElementById(`carriage`).innerText = Math.floor(Math.random()* 20 + 1);
+        document.getElementById(`code`).innerText = Math.floor(Math.random()* 99999 + 1);
+    }
+})
+
+annullare.addEventListener(`click`, function(){
+    const annullare = document.getElementById(`annullare`);
     
+    const santino = document.getElementById(`santino`);
+    santino.classList.remove(`d-none`);
 })
